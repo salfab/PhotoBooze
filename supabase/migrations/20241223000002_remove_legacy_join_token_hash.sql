9b7ef932f2905d0eb5575eeb13058a544a4f7017
Fabio Salvalai <fabio@seesharp.ch>
2025-12-23 14:14:34 +0100

    Fix migration to use DROP COLUMN IF EXISTS

--- supabase/migrations/20241223000002_remove_legacy_join_token_hash.sql
+++ supabase/migrations/20241223000002_remove_legacy_join_token_hash.sql
@@ -1,2 +1,2 @@
 -- Remove legacy join_token_hash column now that we use party_join_tokens table
-ALTER TABLE parties DROP COLUMN join_token_hash;
+ALTER TABLE parties DROP COLUMN IF EXISTS join_token_hash;
